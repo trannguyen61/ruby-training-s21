88f1b19fe2fb60adcb326cfc63b7ceec703fccfb
Tran Nguyen <trannguyen61st@gmail.com>
2021-07-21 14:02:58 +0700

    module2 ruby

---
+++ ruby-exercises/ruby-e1.rb
@@ -0,0 +1,9 @@
+input = gets.chomp
+p input
+splitted_input = input.split.map {|name| name[0].upcase + name[1..]}
+
+last_name = splitted_input[0..-2].map {|name| name[0]}
+first_name = splitted_input[-1]
+
+res = "#{first_name}#{last_name.join('')}"
+p res
---
+++ ruby-exercises/ruby-e2.rb
@@ -0,0 +1,15 @@
+begin
+    n = Integer(gets.chomp)
+rescue ArgumentError => exception
+    p 'Please insert a number.'
+else
+    array = []
+
+    n.times do
+        array.push(gets.chomp)
+    end
+
+    hash = {}
+    array.each {|num| hash.key?(num) ? hash[num] += 1 : hash[num] = 1}
+    p hash.sort_by {|key, value| value}.to_h
+end
